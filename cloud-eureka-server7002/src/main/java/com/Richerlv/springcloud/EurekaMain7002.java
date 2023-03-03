0336de4d26eb8d63116126ccc7cc43afa8dac3cf
Richerlv <richerlv@foxmail.com>
2023-03-04 00:33:16 +0800

    '注册Eureka集群，修改hosts文件，相互注册'

---
+++ cloud-eureka-server7002/src/main/java/com/Richerlv/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.Richerlv.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: Richerlv
+ * @date: 2023/3/4 0:04
+ * @description:
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
--- springclould.md
+++ springclould.md
@@ -13,3 +13,16 @@ Partition tolerance  分区容错性（由于机房网络、分区等原因会�
 ​			AP-数据可能不一致
 
 ZK遵循CP原则， Eureka遵循AP高可用
+
+
+
+#### 配置Eureka集群
+
+要修改映射：C:\Windows\System32\drivers\etc\hosts
+
+按这个教程修改：[如何修改Hosts文件-百度经验 (baidu.com)](https://jingyan.baidu.com/article/335530dafcb63719cb41c3ad.html)
+
+```markdown
+原理：`Hosts是一个没有扩展名的系统文件，主要作用是定义IP地址和主机名的映射关系，就是将一些常用的域名网址与其对应的IP地址建立一个关联“数据库”，当用户在浏览器中输入一个需要登录的网址时，系统会首先自动从Hosts文件中寻找对应的IP地址，如果找到，系统会立即打开对应网页，若没有找到，则系统会再将网址提交DNS域名解析服务器进行IP地址的解析，若发现是被屏蔽的IP或域名，就会禁止打开此网页！`
+```
+
