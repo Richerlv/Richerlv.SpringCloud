0ed2cd86052f929e56a0f6bdfdb8df4ae7ea978a
Richerlv <richerlv@foxmail.com>
2023-03-04 15:33:19 +0800

    'Discovery服务发现'

--- cloud-provider-payment8001/src/main/java/com/Richerlv/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/Richerlv/springcloud/controller/PaymentController.java
@@ -6,7 +6,12 @@ import com.Richerlv.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author: Richerlv
@@ -24,10 +29,13 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @PostMapping("/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
-//        log.info("插入结果:{}", result);
+        log.info("插入结果:{}", result);
         if(result > 0) {
             return new CommonResult<Integer>(200, "插入数据库成功: " + serverPort, result);
         } else {
@@ -39,11 +47,26 @@ public class PaymentController {
     @GetMapping("/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         Payment result = paymentService.getPaymentById(id);
-//        log.info("查询结果:{}", result);
+        log.info("查询结果:{}", result);
         if(result != null) {
             return new CommonResult<Payment>(200, "查询成功: " + serverPort, result);
         } else {
             return new CommonResult<Payment>(444, "没有相应记录, id = " + id, result);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for(String service : services) {
+            log.info("******element: " + service);
+        }
+        List<ServiceInstance> instances = (List<ServiceInstance>) discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+
+        return this.discoveryClient;
+
     }
 }
--- cloud-provider-payment8002/src/main/java/com/Richerlv/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/Richerlv/springcloud/controller/PaymentController.java
@@ -6,7 +6,12 @@ import com.Richerlv.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author: Richerlv
@@ -23,6 +28,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
@@ -45,5 +53,18 @@ public class PaymentController {
         } else {
             return new CommonResult<Payment>(444, "没有相应记录, id = " + id, result);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for(String service : services) {
+            log.info("******element: " + service);
+        }
+        List<ServiceInstance> instances = (List<ServiceInstance>) discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
